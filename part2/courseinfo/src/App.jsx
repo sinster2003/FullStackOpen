cb5c55e5b0b3caf27376b31fa53ef03b5dfdb64a
sinster2003 <sindhurvshabaraya2318@gmail.com>
2023-10-15 22:56:36 +0530

    Exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,50 @@
+const Header = ({title}) => <h1>{title}</h1>
+
+const Part = ({content}) => <p>{content.name} {content.exercises}</p>
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      {
+        parts.map(part => <Part key={part.id} content={part}/>)
+      }
+    </div>
+  );
+}
+
+const Course = ({course}) => {
+  return(
+    <div>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+    </div>
+  );
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
