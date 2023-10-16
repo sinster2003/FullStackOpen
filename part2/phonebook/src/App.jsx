8bf026c81a84f524e582147ba47460914fca66ce
sinster2003 <sindhurvshabaraya2318@gmail.com>
2023-10-16 20:23:24 +0530

    Exercise 2.9 phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { 
-      name: "Arto Hellas",
-      phone: "040-1234567" 
-    }
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
   ]);
   const [newName, setNewName] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [filterValue, setFilterValue] = useState("");
 
   const handleName = (event) => {
     setNewName(event.target.value);
@@ -16,6 +17,10 @@ const App = () => {
 
   const handleNumber = (event) => {
     setPhoneNumber(event.target.value)
+  }
+
+  const handleFilter = (event) => {
+    setFilterValue(event.target.value);
   }
 
   const handlePhone = (event) => {
@@ -27,7 +32,8 @@ const App = () => {
 
       const personObject = {
         name : newName,
-        phone: phoneNumber
+        phone: phoneNumber,
+        id: persons.length + 1
       }
 
       setPersons(persons.concat(personObject));
@@ -39,11 +45,22 @@ const App = () => {
     setNewName("");
     setPhoneNumber("");
   }
+  
+  const personsToShow = filterValue === "" ? persons :
+  persons.filter(person => 
+    person.name.toLowerCase().includes(filterValue.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with <input value={filterValue} onChange={handleFilter}/>
+      </div>
+
       <form onSubmit={handlePhone}>
+        <h2>add a New</h2>
         <div>
           name: <input value={newName} onChange={handleName} required/>
         </div>
@@ -56,7 +73,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(person => <p key={person.name}>{person.name} {person.phone}</p>)
+        personsToShow.map(person => <p key={person.id}>{person.name} {person.phone}</p>)
       }
     </div>
   )
